6ceb705723e1ff010c83ae71043a69c151e3bc4a
Eduardo Borges Siqueira <eduardo@cliente-200-135-86-86.wireless.ufsc.br>
2022-09-06 12:08:22 -0300

    Initial commit

---
+++ cpu.cc
@@ -0,0 +1,27 @@
+#include "cpu.h"
+#include <iostream>
+
+__BEGIN_API
+
+void CPU::Context::save()
+{
+    getcontext(&_context);
+}
+
+void CPU::Context::load()
+{
+    setcontext(&_context);
+}
+
+CPU::Context::~Context()
+{
+    if(_stack)
+        delete _stack;
+}
+
+void CPU::switch_context(Context *from, Context *to)
+{
+    swapcontext(&from->_context, &to->_context);
+}
+
+__END_API
---
+++ cpu.h
@@ -0,0 +1,61 @@
+#ifndef cpu_h
+#define cpu_h
+#define _XOPEN_SOURCE 
+
+#include <ucontext.h>
+#include <iostream>
+#include "traits.h"
+
+__BEGIN_API
+
+class CPU
+{
+    public:
+
+        class Context
+        {
+        private:
+            static const unsigned int STACK_SIZE = Traits<CPU>::STACK_SIZE;
+        public:
+            Context() { _stack = 0; }
+
+            template<typename ... Tn>
+            Context(void (* func)(Tn ...), Tn ... an){
+                getcontext(&_context);
+
+                // Aloca a pilha (importante ser char para trabalhar com bytes)
+                _stack = new char[STACK_SIZE];
+                
+                // Define tamanho da pilha
+                _context.uc_stack.ss_size = STACK_SIZE; 
+                // Define ponteiro para o topo da pilha
+                _context.uc_stack.ss_sp = &_stack;
+                // Especifica o estado da pilha
+                _context.uc_stack.ss_flags=0;
+                // Define o contexto que será retomado
+                _context.uc_link = 0;
+
+                makecontext(&_context, (void(*)())func, sizeof ...(Tn), an...);
+            }
+
+            ~Context();
+
+            void save();
+            void load();
+
+        private:            
+            char *_stack;
+        public:
+            ucontext_t _context;
+        };
+
+    public:
+
+        static void switch_context(Context *from, Context *to);
+
+};
+
+__END_API
+
+#endif
+
